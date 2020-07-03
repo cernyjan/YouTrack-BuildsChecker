559f3552f32b3c604e063ecf22dc3afd473b733d
Jan Černý <j.cerny@quadient.com>
2020-07-03 08:35:25 +0200

    fixed bug: Stopped working with YouTrack Build 2020.2.7179 (redesign)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # YouTrack Builds Checker
 This add-on injects JavaScript into YouTrack web pages for checking untested versions. 
 
-Compatible with YouTrack *2018.4 Build 48733* and lower.
+Compatible with YouTrack Build *2020.2.7179* and newer.
 
 Before add-on: 
 ![before](Before.png)
@@ -10,36 +10,14 @@ After add-on:
 ![after](After.png)
 
 ## Release Notes
-### v1.0.6.0
-* fixed bug: Stopped working with YouTrack 2019.1
-
-### v1.0.5.0
-* fixed bug: Stopped working with YouTrack 2018.4
-
-### v1.0.4.0
-* fixed bug: Does not work with YouTrack 2018.3 Build 46358
-
-### v1.0.3.0
-* fixed bug: Does not detect builds from different distribution
-
-### v1.0.2.0
-* fixed bug: Does not working with two and more places for a number
-
-### v1.0.1.0
-* fixed bug: Does not working on YouTrack 2018.2 Build 44329 
-* changed style of highlighting
-
-### v1.0.0.1
-* fixed bug: when no verified, fixed are not red 
-
-### v1.0.0.0
-* bumped version 
+### v2.0.0.0
+* fixed bug: Stopped working with YouTrack Build 2020.2.7179 (redesign) 
 
 ## Add-on for FireFox
-[YouTrack Builds Checker v1.0.6.0](https://addons.mozilla.org/cs/firefox/addon/youtrack-builds-checker/)
+[YouTrack Builds Checker v2.0.0.0](https://addons.mozilla.org/cs/firefox/addon/youtrack-builds-checker/)
 
 ## Add-on for Chrome
-* download .xpi file from [YouTrack Builds Checker v1.0.6.0](https://github.com/cernyjan/YouTrack-BuildsChecker/releases/tag/v1.0.6.0)
+* download .xpi file from [YouTrack Builds Checker v2.0.0.0](https://github.com/cernyjan/YouTrack-BuildsChecker/releases/tag/v2.0.0.0)
 * extract it on the file system
 * open Extensions page in browser
 * click the “Developer mode” checkbox to activate it
--- YouTrack-BuildsChecker.js
+++ YouTrack-BuildsChecker.js
@@ -6,152 +6,74 @@ $( document ).ready(function() {
 	}
 
 	function checkBuilds() {
-		var year = 2018;
-		var version = 3;
-		var ytVersion = $('a:contains("YouTrack ")').text().split(" ")[1].split(".");
-		if (((parseInt(ytVersion[0]) == year ) && (parseInt(ytVersion[1])>= version )) || ((parseInt(ytVersion[0]) > year )))
-		{
-			//after redesign
-			FixedInVersionYt =  $('span:contains("Fixed in Version")').parent().next('td').children("span").find("span[data-user-id]");
+		//after redesign
+		FixedInVersionYt =  $('span:contains("Fixed in Version")').parent().next('td').children("span").find("span[data-user-id]");
 			
-			VerifiedInVersionYt =  $('span:contains("Verified in Version")').parent().next('td').children("span").find("span[data-user-id]");
-			var VerifiedInVersionArray = [];
-			VerifiedInVersionYt.each(function(){
-			    VerifiedInVersionArray.push($(this).text());
-			});
+		VerifiedInVersionYt =  $('span:contains("Verified in Version")').parent().next('td').children("span").find("span[data-user-id]");
+		var VerifiedInVersionArray = [];
+		VerifiedInVersionYt.each(function(){
+			VerifiedInVersionArray.push($(this).text());
+		});
 
-			FixedInVersionYt.each(function(){
-				var FixedInVersion = $(this).text()
-			    if (VerifiedInVersionArray[0] != "No verified in version") {
-					//compare Fix and Verify
-					var BreakException = {};
-					FixedInVersionSpitted = FixedInVersion.split(" ");
-					FixedInVersionDistribution = FixedInVersionSpitted[0];
-					FixedInVersionNumbersArray = FixedInVersionSpitted[1].split(".");
-					found = false;
-					if (VerifiedInVersionArray.indexOf(FixedInVersion) > -1){
-						found = true;
-					}
-					else{
-						try {
-							VerifiedInVersionArray.forEach(function(VerifiedInVersion) {
-								VerifiedInVersionSpitted = VerifiedInVersion.split(" ");
-								VerifiedInVersionDistribution = VerifiedInVersionSpitted[0];
-								if (FixedInVersionDistribution == VerifiedInVersionDistribution)
-								{
-									VerifiedInVersionNumbersArray = VerifiedInVersionSpitted[1].split(".");
-									if ((parseInt(VerifiedInVersionNumbersArray[0]) == parseInt(FixedInVersionNumbersArray[0])) && (parseInt(VerifiedInVersionNumbersArray[1]) == parseInt(FixedInVersionNumbersArray[1]))){
-										if(parseInt(VerifiedInVersionNumbersArray[2]) > parseInt(FixedInVersionNumbersArray[2])){
-											found = true;
-											throw BreakException;
-										}
-										else if((parseInt(VerifiedInVersionNumbersArray[2]) == parseInt(FixedInVersionNumbersArray[2])) && (parseInt(VerifiedInVersionNumbersArray[3]) >= parseInt(FixedInVersionNumbersArray[3]))) {
-											found = true;
-											throw BreakException;
-										}	
+		FixedInVersionYt.each(function(){
+			var FixedInVersion = $(this).text()
+			if (VerifiedInVersionArray[0] != "No verified in version") {
+				//compare Fix and Verify
+				var BreakException = {};
+				FixedInVersionSpitted = FixedInVersion.split(" ");
+				FixedInVersionDistribution = FixedInVersionSpitted[0];
+				FixedInVersionNumbersArray = FixedInVersionSpitted[1].split(".");
+				found = false;
+				if (VerifiedInVersionArray.indexOf(FixedInVersion) > -1){
+					found = true;
+				}
+				else{
+					try {
+						VerifiedInVersionArray.forEach(function(VerifiedInVersion) {
+							VerifiedInVersionSpitted = VerifiedInVersion.split(" ");
+							VerifiedInVersionDistribution = VerifiedInVersionSpitted[0];
+							if (FixedInVersionDistribution == VerifiedInVersionDistribution)
+							{
+								VerifiedInVersionNumbersArray = VerifiedInVersionSpitted[1].split(".");
+								if ((parseInt(VerifiedInVersionNumbersArray[0]) == parseInt(FixedInVersionNumbersArray[0])) && (parseInt(VerifiedInVersionNumbersArray[1]) == parseInt(FixedInVersionNumbersArray[1]))){
+									if(parseInt(VerifiedInVersionNumbersArray[2]) > parseInt(FixedInVersionNumbersArray[2])){
+										found = true;
+										throw BreakException;
 									}
+									else if((parseInt(VerifiedInVersionNumbersArray[2]) == parseInt(FixedInVersionNumbersArray[2])) && (parseInt(VerifiedInVersionNumbersArray[3]) >= parseInt(FixedInVersionNumbersArray[3]))) {
+										found = true;
+										throw BreakException;
+									}	
 								}
-							});
-						} 
-						catch (e) {
-						  if (e !== BreakException) throw e;
-						}
-					}
-					if (!found) {
-						if (FixedInVersion != "No fixed in version") 
-						{
-							$(this).css('background-color', "red");
-							$(this).css('color', "white");
-						}
-					}
-					else
-					{
-						$(this).css('background-color', "");
-						$(this).css('color', "");
+							}
+						});
+					} 
+					catch (e) {
+					  if (e !== BreakException) throw e;
 					}
 				}
-				else
-				{
+				if (!found) {
 					if (FixedInVersion != "No fixed in version") 
 					{
 						$(this).css('background-color', "red");
 						$(this).css('color', "white");
 					}
-				}    
-			});
-		}
-		else
-		{
-			//old version of YT
-			FixedInVersionYt =  $('div:contains("Fixed in Version")').parent().next('td').children("div").children("a");
-			FixedInVersionArray =  $( FixedInVersionYt ).attr( "title" ).replace("Fixed in Version: ", "").split(',').map(function(item) {
-	  			return item.trim();
-			})
-			
-			if (FixedInVersionArray[0] != "No fixed in version") {
-				VerifiedInVersionArray =  $('div:contains("Verified in Version")').parent().next('td').children("div").children("a").attr( "title" ).replace("Verified in Version: ", "").split(',').map(function(item) {
-			  		return item.trim();
-				});
-				checkResultText = "";
-				if (VerifiedInVersionArray[0] != "No verified in version") {
-					//compare Fix and Verify
-					var BreakException = {};
-					FixedInVersionArray.forEach(function(FixedInVersion) {
-						FixedInVersionSpitted = FixedInVersion.split(" ");
-						FixedInVersionDistribution = FixedInVersionSpitted[0];
-						FixedInVersionNumbersArray = FixedInVersionSpitted[1].split(".");
-						found = false;
-						if (VerifiedInVersionArray.indexOf(FixedInVersion) > -1){
-							found = true;
-						}
-						else{
-							try {
-								VerifiedInVersionArray.forEach(function(VerifiedInVersion) {
-									VerifiedInVersionSpitted = VerifiedInVersion.split(" ");
-									VerifiedInVersionDistribution = VerifiedInVersionSpitted[0];
-									if (FixedInVersionDistribution == VerifiedInVersionDistribution)
-									{
-										VerifiedInVersionNumbersArray = VerifiedInVersionSpitted[1].split(".");
-										if ((parseInt(VerifiedInVersionNumbersArray[0]) == parseInt(FixedInVersionNumbersArray[0])) && (parseInt(VerifiedInVersionNumbersArray[1]) == parseInt(FixedInVersionNumbersArray[1]))){
-											if(parseInt(VerifiedInVersionNumbersArray[2]) > parseInt(FixedInVersionNumbersArray[2])){
-												found = true;
-												throw BreakException;
-											}
-											else if((parseInt(VerifiedInVersionNumbersArray[2]) == parseInt(FixedInVersionNumbersArray[2])) && (parseInt(VerifiedInVersionNumbersArray[3]) >= parseInt(FixedInVersionNumbersArray[3]))) {
-												found = true;
-												throw BreakException;
-											}	
-										}
-									}
-								});
-							} 
-							catch (e) {
-							  if (e !== BreakException) throw e;
-							}
-						}
-						if (!found) {
-							checkResultText = checkResultText + "<span class='c20'>" + FixedInVersion + "&nbsp;" + "</span>, ";
-						}
-						else
-						{
-							checkResultText = checkResultText + FixedInVersion + "&nbsp;" + ", ";
-						}
-					});
 				}
 				else
 				{
-					FixedInVersionArray.forEach(function(FixedInVersion) {
-						checkResultText = checkResultText + "<span class='c20'>" + FixedInVersion  + "&nbsp;" + "</span>, ";
-					});
+					$(this).css('background-color', "");
+					$(this).css('color', "");
 				}
-
-				$( FixedInVersionYt ).text("");
-				if (checkResultText.length > 0){
-					checkResultText = checkResultText.substr(0, checkResultText.length - 2)
-				}
-				$( FixedInVersionYt ).append(checkResultText);
 			}
-		}
+			else
+			{
+				if (FixedInVersion != "No fixed in version") 
+				{
+					$(this).css('background-color', "red");
+					$(this).css('color', "white");
+				}
+			}    
+		});
 	}
 
 	iteration(false);
